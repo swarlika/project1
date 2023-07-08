9e227aa7291df2d3b07bf7c1639e2b5f620cc5cf
bob2 <project2@email.com>
2023-07-08 12:39:30 +0530

    added html code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <html><h1>HELLO THIS IS PROJECT1</h1></html>
         </p>
         <a
           className="App-link"
